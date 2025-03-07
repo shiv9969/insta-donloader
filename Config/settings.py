035ceebf896d3bd7ad8ac8006ce9fb7c66432fbe
shiv9969 <150778501+shiv9969@users.noreply.github.com>
2025-03-07 14:04:46 +0530

    Update settings.py

--- Config/settings.py
+++ Config/settings.py
@@ -1,5 +1,5 @@
 import os
 
-API_ID = os.getenv("API_ID", "your_api_id")
-API_HASH = os.getenv("API_HASH", "your_api_hash")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
+API_ID = os.getenv("API_ID", "9301087")
+API_HASH = os.getenv("API_HASH", "cbabdb3f23de6326352ef3ac26338d9c")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "6473122623:AAFF5upYe3g2z2XTlRnNnIQuG3yDqfcWKTA")
